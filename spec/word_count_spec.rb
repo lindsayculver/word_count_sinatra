eb1dd269937c492b8e736722854146c807d89fd0
Lindsay <leculver@gmail.com>
2015-08-07 10:03:43 -0700

    take a phrase and a seach word with upcase or downcase letters and return the count of words that match

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -7,10 +7,14 @@ describe ('String#word_count') do
   end
 
   it('takes a string/phrase, takes a search word, then returns how often the search word appears') do
-    expect(("I am thankful for my thankful family and thankful friends").word_count("thankful")).to(eq(3))
+    expect(("i am thankful for my thankful family and thankful friends").word_count("thankful")).to(eq(3))
   end
 
   it('takes a string/phrase containing uppercase and lowercase letters and returns the the number count of search word appearances in tha string/phrase') do
     expect(("I Am tHanKfUl FOR my ThanKfUl fAmIly aNd tHANkFul FriENdS").word_count("thankful")).to(eq(3))
+  end
+
+  it('takes an upcase or downcase string/phrase and an upcase search word and returns the number count of the words that match the search word') do
+    expect(("i am thankful for my thankful family and my thankful friends").word_count("THANKFUL")).to(eq(3))
   end
 end
