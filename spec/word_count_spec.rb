dd7f635c0a4575c8bf6222e90553f7dee9951a48
Lindsay <leculver@gmail.com>
2015-08-07 09:53:27 -0700

    process a string and a search word entered by the user and return the number of times the search word appears

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,12 @@ describe ('String#word_count') do
   it('takes a word, matches it, and returns how frequently that word appears') do
     expect(('thankful').word_count('thankful')).to(eq(1))
   end
+
+  it('takes a string/phrase, takes a search word, then returns how often the search word appears') do
+    expect(("I am thankful for my thankful family and thankful friends").word_count("thankful")).to(eq(3))
+  end
+
+  it('takes a string/phrase containing uppercase and lowercase letters and returns the the number count of search word appearances in tha string/phrase') do
+    expect(("I Am tHanKfUl FOR my ThanKfUl fAmIly aNd tHANkFul FriENdS").word_count("thankful")).to(eq(3))
+  end
 end
